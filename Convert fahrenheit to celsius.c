bf2018fb2b1194848bcc566561c35a098898a040
Vanshika694 <75655663+Vanshika694@users.noreply.github.com>
2021-02-22 19:18:32 +0530

    Update Convert fahrenheit to celsius.c

--- Convert fahrenheit to celsius.c
+++ Convert fahrenheit to celsius.c
@@ -1,3 +1,4 @@
+AIM:Write a program to compute Fahrenheit from centigrade (f=1.8*c +32) 
 /* 
 5. Write a C program to compute Fahrenheit from centigrade (f=1.8*c +32)
 */
@@ -6,10 +7,10 @@
 
 int main() 
 {
- float F,C;
- printf("Enter Temperature in Celsius : " );
- scanf("%f",&C);
- F = (C * 1.8) + 32;
- printf("\n %.2f Celsius = %.2f Fahrenheit",C, F);
- return 0;
+   float F,C;
+   printf("Enter Temperature in Celsius : " );
+   scanf("%f",&C);
+   F = (C * 1.8) + 32;
+   printf("\n %.2f Celsius = %.2f Fahrenheit",C, F);
+   return 0;
 }
