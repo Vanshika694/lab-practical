04d314fe13f0a0c690286dde966047493a30e235
Vanshika694 <75655663+Vanshika694@users.noreply.github.com>
2021-02-22 19:22:16 +0530

    Update calculater.c

--- calculater.c
+++ calculater.c
@@ -1,11 +1,6 @@
-/******************************************************************************
+AIM:Write a program to that performs as calculator.
+(Addition, Multiplication, Division and Subtraction).
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 /* 
 1. Write a program to that performs as calculator:(addition, multiplication, division, subtraction).  
 */
